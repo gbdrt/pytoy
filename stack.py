bcc8a090303ca700239393fcb04224735fb0a2cb
Guillaume Baudart <guillaume.baudart@inria.fr>
2023-09-13 15:07:55 +0200

    Stack the ultimate!

--- stack.py
+++ stack.py
@@ -1,3 +1,13 @@
 """
 The ultimate stack!
 """
+
+class Stack:
+    def __init__(self):
+        self.empty = True
+    
+    def isempty(self):
+        return self.empty
+    
+    def push(self, v):
+        self.empty = False
--- test_stack.py
+++ test_stack.py
@@ -1,1 +1,9 @@
 from stack import *
+
+def test_stack():
+    assert Stack().isempty()
+
+def test_isempty():
+    s = Stack()
+    s.push(42)
+    assert not s.isempty()
